32ce32eebcada785fb33294a618936184a6e18db
Dmytro <martyniuk@protonmail.com>
2022-01-29 20:55:22 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@ set -e
 pip install python-telegram-bot
 if  ! pgrep -x "python3" > /dev/null
 then
-    echo "Stopped"
-    cd /workspace
-    ls -l
     python3 bot.py || true
     echo "Started";
 fi
